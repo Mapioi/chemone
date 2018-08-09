968643e99dfa87e2700015ae733ce78f5fded431
杨竞颉 <j.yang19@ejm.org>
2018-08-09 01:03:35 -0700

    Prototype app layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <label>
+        <input
+          autofocus
+          :value="input"
+          placeholder="Parisi"
+        />
+      </label>
+    </div>
+    <div>
+      {{ symbols }}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      input: "",
+      symbols: [["C", "He", "Mo", "Ne"]]
+    }
+  },
+  components: {}
 }
 </script>
 
