199e4532f6421c4c295aac01ced3ba4cea62920e
杨竞颉 <j.yang19@ejm.org>
2018-08-10 17:33:23 +0200

    Tweak app prototype

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,39 @@
       <label>
         <input
           autofocus
-          :value="input"
+          v-model="input"
           placeholder="Parisi"
         />
       </label>
     </div>
     <div>
-      {{ symbols }}
+      <!--TODO render with component-->
+      {{ symbolsList }}
     </div>
   </div>
 </template>
 
 <script>
 
+function spell(word) {
+  return [Array(word)];
+}
+
 export default {
   name: 'app',
+  // NOTE:
+  // "Don’t use arrow functions on an instance property or callback.
+  // As arrow functions are bound to the parent context,
+  // this will not be the Vue instance as you’d expect and this.myMethod will be undefined."
   data() {
     return {
       input: "",
-      symbols: [["C", "He", "Mo", "Ne"]]
     }
+  },
+  computed: {
+    symbolsList() {
+      return spell(this.input);
+    },
   },
   components: {}
 }
