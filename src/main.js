b2f6b6a281a842aea9bad21784d2c46d73ace97e
杨竞颉 <j.yang19@ejm.org>
2018-08-13 16:23:24 +0200

    Prepare for deploying into production

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
 
-Vue.config.productionTip = true;
+Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 const router = new VueRouter({
